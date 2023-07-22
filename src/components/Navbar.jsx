8f3145ff35030cfb6853e623c2e4709ac3a9615e
Georgy Feoktistov <georgijfeoktistov1@gmail.com>
2023-07-22 16:05:27 +0500

    first sample

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div>
+        <a href="#c">Profile</a>
+      </div>
+      <div>
+        <a href="#c">Messages</a>
+      </div>
+      <div>
+        <a href="#c">Photo</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div className="imag">
+        <img src="https://avatars.mds.yandex.net/i?id=ee7463258e1ad1d1191d8783982d893b1c6ec1fe-9699933-images-thumbs&n=13" />
+      </div>
+      <div>ava + description</div>
+      <div>my post</div>
+      <div>new post</div>
+      <div>post 1</div>
+      <div>post 2</div>
+    </div>
+  );
+};
+
+export default Profile;
