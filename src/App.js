9185ebbd40e77b0c115c7e3cc7d9d6df03d7d78d
Georgy Feoktistov <georgijfeoktistov1@gmail.com>
2023-07-28 21:17:01 +0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
 
 
-function App() {
+function App(props) {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            {/*<Profile/>*/}
-            <div className='app-wrapper-content'>
-                <Dialogs/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path={'/dialogs'} element={<Dialogs />} />
+                        <Route path={'/profile'} element={<Profile />} />
+                        <Route path={'/music'} element={<Music />} />
+                        <Route path={'/news'} element={<News />} />
+                        <Route path={'/settings'} element={<Settings />} />
+                    </Routes>
+                </div>
             </div>
-
-        </div>
+        </BrowserRouter>
     );
 }
 
