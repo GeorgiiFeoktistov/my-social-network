f908c0ce8deaa9ea6b09ca780de9bb12d938a204
Georgy Feoktistov <georgijfeoktistov1@gmail.com>
2023-07-28 18:38:17 +0500

    Dialogs was created

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from './MyPosts/MyPosts'
 
 const Profile = () => {
   return (
-    <div className={s.content}>
+    <div>
       <div>
         <img src="https://avatars.mds.yandex.net/i?id=ee7463258e1ad1d1191d8783982d893b1c6ec1fe-9699933-images-thumbs&n=13" />
       </div>
